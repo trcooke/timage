7a8bce7bfef59f286d5fc09c5b8199ad0353e6cc
Tim Cooke <tim@trcooke.co.uk>
2018-01-25 00:05:57 +0000

    Untested changes. Probably won't work so don't trust it.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.mikera/imagez "0.10.0"]]
+                 [net.mikera/imagez "0.12.0"]]
   :main ^:skip-aot timage.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/timage/core.clj
+++ src/timage/core.clj
@@ -1,9 +1,15 @@
 (ns timage.core
   (:gen-class)
-  (:use mikera.image.core))
-
-(def kat (load-image-resource "../resources/lambda-kat-250.png"))
+  (:use mikera.image.core)
+  (:use mikera.image.filters)
+  (:use mikera.image.colours))
 
 (defn -main
   [& args]
-  (show kat))
+  (def kat (load-image-resource "../resources/lambda-kat-250.png"))
+  (def kat2 (copy kat))
+  (def pixels (get-pixels kat2))
+  (dotimes [i 1024]
+    (aset pixels i (rand-colour)))
+  (set-pixels kat2 pixels)
+  (save kat2 "output.png" :quality 0.9 :progressive true))
